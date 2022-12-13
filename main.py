070652a8f10e690ece27be713618a979cc460b49
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 03:13:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import xlrd
 import openpyxl
 import pandas as pd   
 st.set_page_config(layout='wide')
-team_list=['中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', 'Dragons']
+team_list=['中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹']
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
 df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
 st.dataframe(df)
