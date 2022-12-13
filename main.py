da0b9b90e1f646229575f6004b5a44663d46f1d2
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 02:58:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import pandas as pd
 st.set_page_config(layout='wide')
 team_list=['中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', 'Dragons']
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
-df = pd.read_excel("teamsdata.xlsx",sheet_name=team_list) 
-st.dataframe(df)
+if option:
+  df = pd.read_excel("teamsdata.xlsx",sheet_name=team_list) 
+  st.dataframe(df)
