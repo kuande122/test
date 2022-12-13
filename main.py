88b83a8ca549de0ac61aff1244a717299216d4c1
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 02:50:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import xlrd 
+import pandas as pd   
 st.set_page_config(layout='wide')
 team_list=['中信兄弟','樂天桃猿','富邦悍將','台鋼雄鷹','統一7-ELEVEn獅', '味全龍']
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
