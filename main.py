75dccb7fc20182248d10d5b9b2626c91612fb216
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 02:45:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.set_page_config(layout='wide',inital_sidebar_state='expanded')
+st.set_page_config(layout='wide')
 team_list=['中信兄弟','樂天桃猿','富邦悍將','台鋼雄鷹','統一7-ELEVEn獅', '味全龍']
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
