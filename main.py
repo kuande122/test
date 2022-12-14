ee398366c407398ee618c53c523dbb117878fd88
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 08:44:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '�
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
 df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
 st.dataframe(df)
-if option:
-  st.write(option)
+
+st.write(option)
