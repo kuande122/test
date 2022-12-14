0983d6fea9292128c025275e6b4f510121518ca1
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 09:29:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '�
 #st.dataframe(df)
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
 Brothers.Brothers()
-
+st.sidebar.selectbox( '選擇球隊？',team_list)
+if st.sidebar.selectbox( '選擇球隊？',team_list):
+  st.write(st.sidebar.selectbox)
