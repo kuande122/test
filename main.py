296e834dc1793e9953356923ff88e7c7be0f206e
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 09:05:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from PIL import Image
 import Brothers
 st.set_page_config(layout='wide')
-={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
+team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
 
 def user():
   option = st.sidebar.selectbox( '選擇球隊？',team_list)
