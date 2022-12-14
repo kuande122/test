37e5d8a2b4ba31ee8dcf0bc72f96f3bddaeb721e
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 08:28:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image
 st.set_page_config(layout='wide')
 team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
-df = pd.read_excel("teamsdata.xlsx",sheet_name=team_list) 
+df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
 st.dataframe(df)
 image = Image.open(option+'.png')
 st.image(image)
