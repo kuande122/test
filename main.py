da7c25d3a41e94ac3c96d99c846f1b5e0fe3c972
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 09:04:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import Brothers
 st.set_page_config(layout='wide')
 team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
 
-def user(option):
+def user():
   option = st.sidebar.selectbox( '選擇球隊？', option)
   df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
   st.dataframe(df)
   return option
-st.write(user(option))
+st.write(option)
