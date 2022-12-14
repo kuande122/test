45cfd04b6045000ec4bb2d16574a53d4789d6193
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 09:05:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import pandas as pd
 from PIL import Image
 import Brothers
 st.set_page_config(layout='wide')
-team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
+={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
 
 def user():
-  option = st.sidebar.selectbox( '選擇球隊？', option)
+  option = st.sidebar.selectbox( '選擇球隊？',team_list)
   df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
   st.dataframe(df)
   return option
