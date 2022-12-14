e97673b0310992686ee460c172f9ec62a3b98f83
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 08:21:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import streamlit as st
 import xlrd 
 import openpyxl
 import pandas as pd   
+from PIL import Image
 st.set_page_config(layout='wide')
 team_list=['中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹']
 option = st.sidebar.selectbox( '選擇球隊？',team_list)
 df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
 st.dataframe(df)
+image = Image.open('.png')
+st.image(image)
