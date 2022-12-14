abd532aac74166992c3d37ed71f1d21cab885f16
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-14 09:16:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ st.set_page_config(layout='wide')
 team_list={'中信兄弟','樂天桃猿','富邦悍將','統一7-ELEVEn獅', '味全龍','台鋼雄鷹'}
 #df = pd.read_excel("teamsdata.xlsx",sheet_name=option) 
 #st.dataframe(df)
-def user():
-  option = st.sidebar.selectbox( '選擇球隊？',team_list)
-  user_data = {option}
-  teams = pd.DataFrame(user_data, index=[0])
-  return teams
-
-df_user = user()
- 
-df_user
 def users():
   option = st.sidebar.selectbox( '選擇球隊？',team_list)
   return option
