6d38aaab6a6f1a8e1bdcc6cb0bd93742ac7fdf1e
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-18 23:21:12 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -29,11 +29,6 @@ def area():
 teams_list=area()
 option = st.sidebar.selectbox('選擇球隊？',teams_list)
 
-legend_list={'Boston Celtics':{'Bill Russell','Larry Bird','Paul Pierce'},'Brooklyn Nets':{'Julius Erving','Jason Kidd','Derrick Coleman'}
-for player in legend_list.values():
-    st.write(player)
-option1=st.sidebar.selectbox('選擇球員？',legend_list)
-
 teams_information.teams_information(option)
 col1,col2=st.columns((6,4))
 with col1:
