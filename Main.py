4a9a693c3be277874dd137c3696c695be9d89a61
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-12-19 23:56:28 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -77,7 +77,7 @@ with col2:
 data_list={"3P%","2P%","FT%","FG%"}
 option1 = st.selectbox('想查看數據？',data_list)
 
-teams_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=option,usecols=["YEAR", option1],index_col='YEAR') 
+teams_data=pd.read_excel("nbateamsdata.xlsx",sheet_name=option) 
 st.dataframe(teams_data)
 league_data=pd.read_excel("nbateamsdata.xlsx",sheet_name='League Average') 
 plt.style.use("ggplot")
